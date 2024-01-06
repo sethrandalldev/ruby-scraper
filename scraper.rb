e10f0350f6a35b88e8a1c324e72c3542f719a7d0
Seth Randall <sethjrandall@gmail.com>
2024-01-06 18:15:32 -0500

    add headlines to csv

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 
 url = 'https://en.wikipedia.org/wiki/Indonesia'
 document = Nokogiri::HTML(URI.open(url))
 
 headlines = document.css('span.mw-headline').map(&:text)
 puts headlines
+
+CSV.open('headlines.csv', 'w') do |csv|
+    headlines.each do |headline|
+        csv << [headline]
+    end
+end
